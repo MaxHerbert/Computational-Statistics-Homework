bff4090262bfece83b274fc0aafac6b70f06b042
max <max.herbert91@gmail.com>
2022-01-05 18:17:09 +0100

    1st draft

--- Script.R
+++ Script.R
@@ -1,5 +1,7 @@
 # import necessary packages
 library(tidyverse)
+library(readxl)
+
 
 # set working directory relative to path of the script file
 setwd(dirname(rstudioapi::getActiveDocumentContext()$path))
@@ -7,7 +9,12 @@ setwd(dirname(rstudioapi::getActiveDocumentContext()$path))
 # import training and test data
 test <- read_csv("data/test.csv")
 train <- read_csv("data/train.csv")
+codebook <- read_excel("data/codebook.xlsx", 
+                       col_types = c("text", "skip", "text"))
+
 
 # remove redundant columns
-test <- test %>% select(-tamhog)
-train <- train %>% select(-tamhog)
+test <- test %>% select(-c(tamhog, Id))
+train <- train %>% select(-c(tamhog, Id))
+
+colnames(test)[which(names(test) == codebook$`Variable name`)] <- codebook$`English Variable Name`
