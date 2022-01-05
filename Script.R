04e8d472b283895ea88df964c6dd5d7ccd3ef5e3
max <max.herbert91@gmail.com>
2022-01-05 15:24:25 +0100

    added wd and data import

---
+++ Script.R
@@ -0,0 +1,9 @@
+# import necessary packages
+library(tidyverse)
+
+# set working directory relative to path of the script file
+setwd(dirname(rstudioapi::getActiveDocumentContext()$path))
+
+# import training and test data
+test <- read_csv("data/test.csv")
+train <- read_csv("data/train.csv")
