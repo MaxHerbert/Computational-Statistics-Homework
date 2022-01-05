4f7ba16325815c472e2f9679e20e301befaaf66e
max <max.herbert91@gmail.com>
2022-01-05 15:51:37 +0100

    remove redundant cols

--- Script.R
+++ Script.R
@@ -7,3 +7,7 @@ setwd(dirname(rstudioapi::getActiveDocumentContext()$path))
 # import training and test data
 test <- read_csv("data/test.csv")
 train <- read_csv("data/train.csv")
+
+# remove redundant columns
+test <- test %>% select(-tamhog)
+train <- train %>% select(-tamhog)
