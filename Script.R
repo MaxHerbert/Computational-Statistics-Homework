33ef47a2e0a2b8de471e738f98a60e0776fed7a7
max <max.herbert91@gmail.com>
2022-01-22 18:13:05 +0100

    edited comment

--- Script.R
+++ Script.R
@@ -84,7 +84,7 @@ val_cart <- validation %>% mutate(predictions = predict(cart, validation, type =
 confusionMatrix(val_cart$predictions, val_cart$target)
 
 
-# predict test set and output frequency of predictions
+# predict test set and output (relative) frequency of predictions
 test_predict <- test %>% mutate(prediction = predict(rf, test))
 pred_freq <- table(test_predict$prediction)
 pred_freq_perc <- pred_freq / sum(!is.na(test_predict$prediction)) * 100
